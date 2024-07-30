e2045ddb2bc1ac767a8e418e7a5a713596ef8cd3
DESKTOP-U0VLU25\Admin <dzung.apr6th@gmail.com>
2024-07-30 11:03:49 +0700

    refactor(move the project to file Extension)

---
+++ VsixContinueExtension/ContinueExtensionVsix/ContinueCodeFixProvider.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ContinueExtensionVsix
+{
+    internal class ContinueCodeFixProvider
+    {
+    }
+}
